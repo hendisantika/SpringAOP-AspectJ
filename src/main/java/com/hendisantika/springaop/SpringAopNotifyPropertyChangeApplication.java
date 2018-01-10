9d41e83ce2db9d08bba3946aacd75a8411d9953d
Hendi Santika <hendisantika@gmail.com>
2018-01-10 07:07:02 +0700

    #2 - Add Main App

---
+++ src/main/java/com/hendisantika/springaop/SpringAopNotifyPropertyChangeApplication.java
@@ -0,0 +1,23 @@
+package com.hendisantika.springaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : SpringAOP-AspectJ
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/01/18
+ * Time: 21.56
+ * To change this template use File | Settings | File Templates.
+ */
+
+@SpringBootApplication
+public class SpringAopNotifyPropertyChangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAopNotifyPropertyChangeApplication.class, args);
+    }
+}
